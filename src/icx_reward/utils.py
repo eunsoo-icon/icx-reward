5ba9aaae1bbe49b79face362deb896377a4f8620
Eunsoo Park <eunsoo.park@parametacorp.com>
2023-10-24 13:51:10 +0900

    Add print_progress()

--- src/icx_reward/utils.py
+++ src/icx_reward/utils.py
@@ -1,4 +1,5 @@
 import json
+import sys
 
 DEBUG = False
 TAB = "\t"
@@ -22,3 +23,14 @@ def pprint(data):
         print(json.dumps(data, indent=4))
     else:
         print(data)
+
+
+def print_progress(iteration, total, prefix='', suffix='', decimals=1, bar_length=100):
+    format_str = "{0:." + str(decimals) + "f}"
+    percent = format_str.format(100 * (iteration / float(total)))
+    filled_length = int(round(bar_length * iteration / float(total)))
+    bar = '#' * filled_length + '-' * (bar_length - filled_length)
+    sys.stdout.write('\r%s |%s| %s%s %s' % (prefix, bar, percent, '%', suffix)),
+    if iteration == total:
+        sys.stdout.write('\n')
+    sys.stdout.flush()
