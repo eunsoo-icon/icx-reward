c778c56ffc865d7e2e4cc61c8d1bdd167c20c49a
Eunsoo Park <eunsoo.park@parametacorp.com>
2023-10-31 09:20:32 +0900

    Change vote event log signature

--- src/icx_reward/types/event.py
+++ src/icx_reward/types/event.py
@@ -1,7 +1,7 @@
 class EventSig:
     Penalty = "PenaltyImposed(Address,int,int)"
     Slash = "Slashed(Address,Address,int)"
-    SetBond = "SetBond(Address,bytes)"
-    SetDelegation = "SetDelegation(Address,bytes)"
+    SetBond = "BondSet(Address,bytes)"
+    SetDelegation = "DelegationSet(Address,bytes)"
     VOTE_SIG_LIST = [SetBond, SetDelegation]
 
