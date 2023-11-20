5ee64dd9b462d59051263a40c06c2b2baea561ae
Eunsoo Park <eunsoo.park@parametacorp.com>
2023-11-20 12:07:43 +0900

    Improve command output with Term information
    - add Term and RewardFund type
    - improve command output with Term information

--- src/icx_reward/commands.py
+++ src/icx_reward/commands.py
@@ -4,6 +4,7 @@ from functools import wraps
 from icx_reward.penalty import PenaltyFetcher
 from icx_reward.rpc import RPC
 from icx_reward.reward import PRepReward, Voter
+from icx_reward.types.term import Term
 from icx_reward.types.exception import InvalidParamsException
 from icx_reward.utils import pprint
 from icx_reward.vote import VoteFetcher
@@ -62,17 +63,15 @@ def fetch_vote(args: dict, height: int, term_: dict):
     uri = args["uri"]
     export_fp = args.get("export")
     address = args["address"]
-    start_height = int(term_["startBlockHeight"], 16)
-    end_height = int(term_["endBlockHeight"], 16)
-    iiss_version = int(term_["iissVersion"], 16)
+    t = Term.from_dict(term_)
 
-    if iiss_version < 4:
+    if t.iiss_version < 4:
         pprint("Can't fetch vote. Support IISS 4 only.")
         return
 
-    pprint(f"## Fetch votes of {'all' if address is None else address} from {start_height} to {end_height}")
+    pprint(f"## Fetch votes of {'all' if address is None else address} in {t.info()}")
     vf = VoteFetcher(uri)
-    vf.fetch(start_height, end_height, address, fp=sys.stdout)
+    vf.fetch(t.start_block_height, t.end_block_height, address, fp=sys.stdout)
     if export_fp is not None:
         print(f"## Export result to {export_fp.name}")
         vf.export(export_fp)
@@ -81,15 +80,14 @@ def fetch_vote(args: dict, height: int, term_: dict):
 
 
 @time_info
-def fetch_penalty(args: dict, height: int, _term: dict):
+def fetch_penalty(args: dict, height: int, term_: dict):
     address = args["address"]
-    start_height = int(_term["startBlockHeight"], 16)
-    end_height = int(_term["endBlockHeight"], 16)
+    t = Term.from_dict(term_)
 
-    pprint(f"## Fetch penalties of {address} from {start_height} to {end_height}")
+    pprint(f"## Fetch penalties of {'all' if address is None else address} in {t.info()}")
     pf = PenaltyFetcher(args["uri"])
     try:
-        penalties = pf.run(start_height, end_height, address, True)
+        penalties = pf.run(t.start_block_height, t.end_block_height, address, True)
     except InvalidParamsException as e:
         pprint(f"{e}")
         return
@@ -104,26 +102,22 @@ def check(args: dict, height: int, term_: dict):
     uri = args["uri"]
     address = args["address"]
     import_fp = args["import"]
-    start_height = int(term_["startBlockHeight"], 16)
-    end_height = int(term_["endBlockHeight"], 16)
-    iiss_version = int(term_["iissVersion"], 16)
+    t = Term.from_dict(term_)
 
-    if iiss_version < 4:
+    if t.iiss_version < 4:
         pprint("Support IISS 4 only.")
         return
 
     rpc = RPC(uri)
-    period = int(term_["period"], 16)
-    event_start_height = start_height - 2 * period
-    event_end_height = end_height - 2 * period
+    et = Term.from_dict(rpc.term(t.start_block_height - 2 * t.period))
 
     print(f"## Check reward of {address} at height {height}\n")
 
     # get all vote events
     vf = VoteFetcher(uri)
     if import_fp is None:
-        print(f"## Fetch all votes from {event_start_height} to {event_end_height}")
-        vf.fetch(event_start_height, event_end_height, fp=sys.stdout)
+        print(f"## Fetch all votes in {et.info()}")
+        vf.fetch(et.start_block_height, et.end_block_height, fp=sys.stdout)
     else:
         print(f"## Import votes from {import_fp.name}")
         vf.import_from_file(import_fp)
@@ -132,8 +126,8 @@ def check(args: dict, height: int, term_: dict):
     print()
 
     # prep reward
-    pr = PRepReward.from_network(uri, event_start_height)
-    print(f"## Calculate reward of elected PReps from {pr.start_height} to {pr.end_height}")
+    pr = PRepReward.from_network(uri, et.start_block_height)
+    print(f"## Calculate reward of elected PReps in {et.info()}")
     pr.calculate(vf.votes)
     pr.print_summary()
 
@@ -152,8 +146,8 @@ def check(args: dict, height: int, term_: dict):
     print(f"\t= {0 if prep is None else prep.commission} + {0 if prep is None else prep.wage} + {voter.reward}")
 
     # query iscore from network
-    iscore = (int(rpc.query_iscore(address, start_height + 1).get("iscore", "0x0"), 16)
-              - int(rpc.query_iscore(address, start_height).get("iscore", "0x0"), 16))
+    iscore = (int(rpc.query_iscore(address, t.start_block_height + 1).get("iscore", "0x0"), 16)
+              - int(rpc.query_iscore(address, t.start_block_height).get("iscore", "0x0"), 16))
 
     print(f"\n## Queried I-Score: {iscore}")
 
